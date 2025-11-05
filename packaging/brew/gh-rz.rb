dbccb685d37956179acf35bfe698a0280cee0970
Rick Ball <41898282+rickballard@users.noreply.github.com>
2025-11-05 16:06:07 -0500

    Packaging stubs: winget/chocolatey/brew

---
+++ packaging/brew/gh-rz.rb
@@ -0,0 +1,10 @@
+# Placeholder Ruby formula for Brew tap
+class GhRz < Formula
+  desc "RepoZipper Cloud: read-only Git-native backup for GitHub"
+  homepage "https://github.com/rickballard/gh-rz"
+  version "0.2.0"
+  # TODO: add URL + SHA256 of the released zip
+  def install
+    odie "fill in once release is final"
+  end
+end
---
+++ packaging/winget/README.md
@@ -0,0 +1,2 @@
+# Placeholder winget manifest (fill in after v0.2.0 confirms)
+# See: https://learn.microsoft.com/windows/package-manager/package/
